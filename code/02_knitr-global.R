234f49a75adcb737b94b71abdece2b7e7164428f
ogorodriguez <ogorodriguez@gmail.com>
2020-12-19 20:10:55 +0100

    Fixed fig path param

--- code/02_knitr-global.R
+++ code/02_knitr-global.R
@@ -1,5 +1,5 @@
 # Global options for all files
 rm(list=ls()) ### To clear namespace
 library(knitr)
-opts_chunk$set(fig.width=12, fig.height=8, fig.path=here::here("data/images"),
+opts_chunk$set(fig.width=12, fig.height=8, fig.path=here::here("data/images/"),
                echo=TRUE, warning=FALSE, message=FALSE)
