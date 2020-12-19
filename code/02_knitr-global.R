ac89fcdf2d8fe6182d293c4648486862e191efec
ogorodriguez <ogorodriguez@gmail.com>
2020-12-19 22:51:30 +0100

    Content 02 created, and several adjustments to source code as well as the data folder

--- code/02_knitr-global.R
+++ code/02_knitr-global.R
@@ -1,5 +1,8 @@
 # Global options for all files
 rm(list=ls()) ### To clear namespace
 library(knitr)
-opts_chunk$set(fig.width=12, fig.height=8),
-               echo=TRUE, warning=FALSE, message=FALSE)
+opts_chunk$set(fig.width=12,
+               fig.height=8,
+               echo=TRUE,
+               warning=FALSE,
+               message=FALSE)
